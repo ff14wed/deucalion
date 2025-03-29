4bca8f9d442e5819a1d62ff7f25f223a3163c64c
Flawed <33593723+ff14wed@users.noreply.github.com>
2025-03-28 19:50:04 -0700

    Update dependencies

--- deucalion/src/hook/packet.rs
+++ deucalion/src/hook/packet.rs
@@ -4,7 +4,7 @@ use std::mem;
 use anyhow::{format_err, Result};
 use binary_layout::prelude::*;
 
-define_layout!(frame_header, LittleEndian, {
+binary_layout!(frame_header, LittleEndian, {
   magic: [u8; 16],
   timestamp: u64,
   size: u32,
@@ -16,7 +16,7 @@ define_layout!(frame_header, LittleEndian, {
   decompressed_length: u32,
 });
 
-define_layout!(segment_header, LittleEndian, {
+binary_layout!(segment_header, LittleEndian, {
   size: u32,
   source_actor: u32,
   target_actor: u32,
@@ -24,12 +24,12 @@ define_layout!(segment_header, LittleEndian, {
   padding: u16,
 });
 
-define_layout!(segment, LittleEndian, {
+binary_layout!(segment, LittleEndian, {
   header: segment_header::NestedView,
   data: [u8],
 });
 
-define_layout!(ipc_header, LittleEndian, {
+binary_layout!(ipc_header, LittleEndian, {
   reserved: u16,
   opcode: u16,
   padding: u16,
@@ -38,18 +38,18 @@ define_layout!(ipc_header, LittleEndian, {
   padding1: u32,
 });
 
-define_layout!(ipc_packet, LittleEndian, {
+binary_layout!(ipc_packet, LittleEndian, {
   header: ipc_header::NestedView,
   data: [u8],
 });
 
-define_layout!(deucalion_segment_header, LittleEndian, {
+binary_layout!(deucalion_segment_header, LittleEndian, {
   source_actor: u32,
   target_actor: u32,
   timestamp: u64,
 });
 
-define_layout!(deucalion_segment, LittleEndian, {
+binary_layout!(deucalion_segment, LittleEndian, {
   header: deucalion_segment_header::NestedView,
   data: [u8],
 });
